cfe3010980e1a9e465b51603e30e23013b27d72b
Antoine Valente <antoinevalente1@gmail.com>
2024-05-17 11:51:31 +0200

    add custom fetch with retry logic on api client

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,27 @@
+import retry from "async-retry";
 import createClient from "openapi-fetch";
 import type { paths } from "./schema";
 
+const fetcher: typeof fetch = (...args) =>
+	retry(
+		async () => {
+			const res = await fetch(...args);
+
+			if (!res.ok) {
+				throw new Error(res.statusText);
+			}
+
+			return res;
+		},
+		{
+			retries: 5,
+			minTimeout: 500,
+		},
+	);
+
 const { GET } = createClient<paths>({
 	baseUrl: process.env.CHAINBASE_API_URL,
+	fetch: fetcher,
 	headers: {
 		"x-api-key": process.env.CHAINBASE_API_KEY,
 	},
